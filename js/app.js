76ef3562f60ff319ecf444e2fc8091be6e22b399
Pablo Rodriguez <arnejado@hotmail.com>
2023-11-16 12:31:30 +0100

    añadiendo botones ce y intro

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ var panel = document.querySelector(".panel");
 // Escuchar los botones con el id igual al número
 var botones = document.querySelectorAll(".boton");
 
+console.log(botones);
+
 // Recorrer los botones y añadir un evento de clic
 for (var i = 0; i < botones.length; i++) {
     botones[i].addEventListener("click", function() {
@@ -19,4 +21,18 @@ for (var i = 0; i < botones.length; i++) {
       // Mostrar el número en el div con la clase "panel"
       panel.textContent = numero;
     });
+  }
+
+  //botón de borrar
+
+  var botonBorrar = document.querySelector(".botonCE");
+
+  console.log(botonBorrar);
+
+  botonBorrar.addEventListener("click", borrar);
+  
+  function borrar () {
+    numero = 0
+    panel.textContent = numero;
+    console.log ("borrando...");
   }
